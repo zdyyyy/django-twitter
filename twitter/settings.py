a8c132704ef0f7daf80c7f9599bede799f3fe891
Ronald Yin <zidyin_jx@126.com>
2023-05-02 15:54:42 -0400

    6-tweet-model

--- twitter/settings.py
+++ twitter/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     #project apps
     'accounts',
+    'tweets'
 ]
 
 REST_FRAMEWORK = {'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
