03159650ccbfedc9120d17f7c7980621737d2e6e
Ronald Yin <zidyin_jx@126.com>
2023-05-02 20:18:35 -0400

    7-tweets-api-and-tests

--- accounts/api/tests.py
+++ accounts/api/tests.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+# from django.test import TestCase
 from rest_framework.test import APIClient
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from testing.testcases import TestCase
 
 LOGIN_URL = '/api/accounts/login/'
 LOGOUT_URL = '/api/accounts/logout/'
@@ -10,13 +11,14 @@ LOGIN_STATUS_URL = '/api/accounts/login_status/'
 class AccountApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = self.createUser(
+        # self.user = self.createUser(
+        self.user = self.create_user(
             username='admin',
             email='admin@jiuzhang.com',
             password="correct password",
         )
-    def createUser(self,username,email,password):
-        return User.objects.create_user(username,email,password)
+    # def createUser(self,username,email,password):
+    #     return User.objects.create_user(username,email,password)
 
     def test_login(self):
 
---
+++ tweets/api/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from tweets.api.serializers import TweetSerializer, TweetCreateSerializer
+from tweets.models import Tweet
+# Create your views here.
+class TweetViewSet(viewsets.GenericViewSet,
+                   viewsets.mixins.CreateModelMixin,
+                   viewsets.mixins.ListModelMixin):
+    queryset = Tweet.objects.all()
+    serializer_class = TweetCreateSerializer
+
+    def get_permissions(self):
+        if self.action == 'list':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def create(self,request,*args,**kwargs):
+        serializer = TweetCreateSerializer(
+            data = request.data,
+            context = {'request':request}
+        )
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message':"Please check input",
+                'errors':serializer.errors,
+            },status = 400)
+        tweet = serializer.save()
+        return Response(TweetSerializer(tweet).data,status = 201)
+
+    def list(self, request, *args,**kwargs):
+        if 'user_id' not in request.query_params:
+            return Response('missing user_id',status = 400)
+
+        tweets = Tweet.objects.filter(
+            user_id = request.query_params['user_id']
+        ).order_by('-created_at')
+        serializer = TweetSerializer(tweets,many = True)
+        return Response({'tweets': serializer.data})
