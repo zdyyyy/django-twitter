fe50d18693d067b3f2a6e7ffc22c1fbe770b3883
Ronald Yin <zidyin_jx@126.com>
2023-05-23 22:11:38 -0400

    34-cache-newsfeed-list-in-redis, needs to fix fields bug

--- newsfeeds/models.py
+++ newsfeeds/models.py
@@ -1,7 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from newsfeeds.listeners import push_newsfeed_to_cache
 from tweets.models import Tweet
 from utils.memcached_helper import MemcachedHelper
+
 
 # Create your models here.
 class NewsFeed(models.Model):
@@ -20,3 +23,6 @@ class NewsFeed(models.Model):
     @property
     def cached_tweet(self):
         return MemcachedHelper.get_object_through_cache(Tweet, self.tweet_id)
+
+
+post_save.connect(push_newsfeed_to_cache,sender = NewsFeed)
